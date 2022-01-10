782af3d6dd529191ca406397540c13bbe5df5b5a
maroqijalil <jalilpro31@gmail.com>
2022-01-10 17:59:08 +0700

    feat: add kansei main for example

---
+++ src/kansei_main.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2021 Ichiro ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <vector>
+
+#include "kansei/measurement/measurement.hpp"
+#include "kansei/fallen/fallen.hpp"
+#include "kansei/node/kansei_node.hpp"
+#include "keisan/keisan.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  auto kansei_node = std::make_shared<kansei::KanseiNode>("kansei_node");
+
+  auto filter = std::make_shared<kansei::measurement::Filter>();
+  auto fallen = std::make_shared<kansei::fallen::FallenDeterminant>(
+    kansei::fallen::DeterminantType::ACCELERO);
+
+  kansei_node->set_measurement_unit(filter);
+  kansei_node->set_fallen_determinant(fallen);
+
+  rclcpp::spin(kansei_node);
+  rclcpp::shutdown();
+
+  return 0;
+}
